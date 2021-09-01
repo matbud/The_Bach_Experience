475fbac32bd1f761c82a13c8e14e3f03c94b6a1e
Elodie Duffau <elodie.duffau@gmail.com>
2021-09-01 12:41:42 +0200

    dashboard start

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,2 +1,11 @@
 class DashboardsController < ApplicationController
+  # before_action :authenticate_user!
+  # include Pundit
+  skip_before_action :authenticate_user!, only: [ :show ]
+
+  def show
+    @event = Event.find(params[:id])
+    authorize @event
+  end
+
 end
