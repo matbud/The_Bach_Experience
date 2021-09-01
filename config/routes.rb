2732c5c8ebf8667adfe6af7f76c26bca863d52de
Mélanie Olivier <85628939+MelanieOlivier@users.noreply.github.com>
2021-09-01 12:39:03 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :events, only: [ :new, :create ] do
-    resources :recommendations, only: [:index, :show]
-  end
+  resources :events, only: [ :new, :create ]
   get '/events/:id/choose_recommendation', to: 'events#choose_recommendation'
   get '/draft', to: 'pages#draft'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
