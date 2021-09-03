ed005a1daf4cf244d84981ca1cb38231527f07ec
Mateusz Budzisz <mat.budzisz@gmail.com>
2021-09-03 15:33:13 +0200

    added methods in event

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,12 @@ class Event < ApplicationRecord
   has_many :messages
   has_many :recommendations
   has_many :guests
+  has_many :plannings
 
   validates :gender, :theme, :start_date, :end_date, :budget_per_person, :number_of_guests, :location, presence: true
   validates :budget_per_person, :number_of_guests, numericality: { greater_than: 0 }
+
+  def available_dates
+    { from: start_date, to: (end_date + 1) }
+  end
 end
