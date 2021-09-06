7a3dd4e9f9095528c37ddf38efc97aebf2e25544
Mateusz Budzisz <mat.budzisz@gmail.com>
2021-09-06 11:41:39 +0200

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_06_083240) do
+ActiveRecord::Schema.define(version: 2021_09_06_093942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_09_06_083240) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "theme"
     t.string "gender"
+    t.string "location"
     t.string "photo_name"
   end
 
@@ -60,6 +61,7 @@ ActiveRecord::Schema.define(version: 2021_09_06_083240) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
