8b17da2cc5e23a961582b55e4a455451812a26bd
Mateusz Budzisz <mat.budzisz@gmail.com>
2021-09-08 18:01:46 +0200

    fixed

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,12 +1,13 @@
 class GuestsController < ApplicationController
   def create
     @event = Event.find(params[:event_id])
-    @emails = params[:emails].split(",")
+    @emails = params[:emails].gsub(" ", "").split(/,|;|&/)
     @emails.each do |email|
       user = User.find_by(email: email)
       guest = Guest.create(status: 'pending', event: @event, user: user)
       authorize guest
     end
+    raise
     redirect_to dashboard_path(current_user.event)
   end
 
