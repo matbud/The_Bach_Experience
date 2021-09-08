1c8793591a5eef9057791935cf554db51c17bf07
MelanieOlivier <melanie.olivier13@gmail.com>
2021-09-08 18:24:52 +0200

    save responsive navbar

--- app/javascript/plugins/init_navbar.js
+++ app/javascript/plugins/init_navbar.js
@@ -1,5 +1,5 @@
-const burger = document.getElementById("burgers-menu")
-const buttons = document.getElementById("buttons-menu")
+// const burger = document.getElementById("burgers-menu")
+// const buttons = document.getElementById("buttons-menu")
 
-function mobileMenu() { burger.classList.toggle("active"); buttons.classList.toggle("active"); }
-function closeMenu() { burger.classList.remove("active"); buttons.classList.remove("active"); }
+// function mobileMenu() { burger.classList.toggle("active"); buttons.classList.toggle("active"); }
+// function closeMenu() { burger.classList.remove("active"); buttons.classList.remove("active"); }
